535d6930d4cc9505b72b29d179fc3ba5654669ba
Rosa Imantoro <iman@alterra.id>
2020-11-22 10:35:20 +0700

    modules: how to call a function inside modules

--- Program.fs
+++ Program.fs
@@ -3,8 +3,12 @@ open System
 let printGreeting name =
   printfn "Hello %s from F#!" name
 
+module BasicFunctions =
+  let sampleFunction1 x = x*x + 3
+  let result1 = sampleFunction1 4573
+  printfn "The result of squaring the integer 4573 and adding 3 is %d" result1
+
 [<EntryPoint>]
 let main argv =
-  // Call your new fuhction
-  printGreeting "Iman"
+  let result = BasicFunctions.sampleFunction1
   0
